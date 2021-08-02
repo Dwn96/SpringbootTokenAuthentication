b2e8e5a6f17802761b9e98be4a9128c6e5a9e793
Dan Njeru <wambu43@gmail.com>
2021-08-02 19:38:30 +0300

    Added fileService dependency injection.

--- src/main/java/io/auth/secure/File/FileService.java
+++ src/main/java/io/auth/secure/File/FileService.java
@@ -2,6 +2,7 @@ package io.auth.secure.File;
 
 import io.auth.secure.user.User;
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
@@ -10,11 +11,18 @@ import java.io.IOException;
 import java.util.Objects;
 import java.util.Optional;
 
-@AllArgsConstructor
 @Service
+
 public class FileService {
+
+
+
     private final FileRepository fileRepository;
     private File file;
+
+    FileService(FileRepository fileRepository){
+        this.fileRepository = fileRepository;
+    }
 
     public void saveImage(MultipartFile multipartFile) throws IOException {
         file.setName(StringUtils.cleanPath(Objects.requireNonNull(multipartFile.getOriginalFilename())));
