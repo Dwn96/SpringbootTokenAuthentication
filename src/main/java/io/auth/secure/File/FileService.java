85821e959ceb7c12e86625832dafac670d06047e
Dan Njeru <wambu43@gmail.com>
2021-08-02 19:29:56 +0300

    Added file repository and  service classes

---
+++ src/main/java/io/auth/secure/File/FileService.java
@@ -0,0 +1,32 @@
+package io.auth.secure.File;
+
+import io.auth.secure.user.User;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Objects;
+import java.util.Optional;
+
+@AllArgsConstructor
+@Service
+public class FileService {
+    private final FileRepository fileRepository;
+    private File file;
+
+    public void saveImage(MultipartFile multipartFile) throws IOException {
+        file.setName(StringUtils.cleanPath(Objects.requireNonNull(multipartFile.getOriginalFilename())));
+        file.setContentType(multipartFile.getContentType());
+        file.setData(multipartFile.getBytes());
+        file.setSize(multipartFile.getSize());
+
+        fileRepository.save(file);
+    }
+
+    public Optional<File> getImage(User user){
+      return fileRepository.findById(user.getId());
+    }
+
+}
