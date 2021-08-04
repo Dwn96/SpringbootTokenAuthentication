b76feaee3b7db272fe24ae0876ffd43ee37a2578
Dan Njeru <wambu43@gmail.com>
2021-08-04 16:12:36 +0300

    Added value to request mapping

--- src/main/java/io/auth/secure/registration/RegistrationController.java
+++ src/main/java/io/auth/secure/registration/RegistrationController.java
@@ -3,8 +3,13 @@ package io.auth.secure.registration;
 import io.auth.secure.File.File;
 import io.auth.secure.File.FileService;
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.ServletRequestDataBinder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.support.ByteArrayMultipartFileEditor;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
 
 @CrossOrigin
 @RestController
@@ -14,8 +19,10 @@ public class RegistrationController {
     private RegistrationService registrationService;
     private FileService fileService;
 
+
+
     @PostMapping
-    public String register (@RequestPart("user") RegistrationRequest request, @RequestPart("file") MultipartFile multipartFile){
+    public String register (@RequestPart(value = "user")  RegistrationRequest request, @RequestPart(value ="file") MultipartFile multipartFile){
                return  registrationService.register(request,multipartFile);
     }
 
